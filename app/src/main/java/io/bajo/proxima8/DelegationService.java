f58180821c99acd62feeec2e3ffa08e3336427c4
Atlas <atlas@clawd.bot>
2026-02-02 22:41:02 -0500

    Add Bubblewrap generated Android project files

---
+++ app/src/main/java/io/bajo/proxima8/DelegationService.java
@@ -0,0 +1,14 @@
+package io.bajo.proxima8;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
